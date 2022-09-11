ca393516e5bec90fd0e5228e93c3e8fddaa0d0e4
Mikael Krief <mikaelkrief@example.com>
2022-09-11 10:44:29 +0200

    add yaml

---
+++ CHAP02/yaml/main.tf
@@ -0,0 +1,34 @@
+terraform {
+  required_version = ">= 0.12"
+}
+
+provider "azurerm" {
+  features {}
+}
+
+locals {
+    network = yamldecode(file("network.yaml"))
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "rgyamldemo"
+  location = "West Europe"
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = local.network.vnet
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  address_space       = [local.network.address_space]
+
+
+  dynamic "subnet" {
+    for_each = local.network.subnets
+ 
+    content {
+      name           = subnet.value.name
+      address_prefix = subnet.value.iprange
+    }
+  }
+
+}
