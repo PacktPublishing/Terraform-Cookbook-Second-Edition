f6a5cf4ab1d393d0db9dba4c09bddf14ec7be670
Radek Simko <radek.simko@gmail.com>
2022-09-19 14:40:13 +0100

    Suggestions for chapter 01

--- CHAP01/install_terraform_linux.sh
+++ CHAP01/install_terraform_linux.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 TERRAFORM_VERSION="1.2.5"
 resp=$(curl -sS https://api.releases.hashicorp.com/v1/releases/terraform/${TERRAFORM_VERSION})
 binaryUrl=$(echo $resp | jq -r '.builds[] | select (.os == "linux" and .arch == "amd64") | .url')
