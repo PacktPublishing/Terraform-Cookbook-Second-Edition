1efcf353a2fbe0f9a549a168a153165daeb4317e
Radek Simko <radek.simko@gmail.com>
2022-10-09 15:00:53 +0100

    chap03: add or correct version constraints

--- CHAP03/myApp/solution1/production/main.tf
+++ CHAP03/myApp/solution1/production/main.tf
@@ -1,6 +1,10 @@
-
 terraform {
-  required_version = ">= 0.12"
+  required_version = "~> 1.1"
+  required_providers {
+    azurerm = {
+      version = "~> 3.23"
+    }
+  }
 }
 
 provider "azurerm" {
