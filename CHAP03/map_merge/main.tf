6b4531acaa55adbfd8f3bba20e16293365f8ecfb
Radek Simko <radek.simko@gmail.com>
2022-10-18 21:11:01 +0100

    map_merge: replace deprecated resources

--- CHAP03/map_merge/main.tf
+++ CHAP03/map_merge/main.tf
@@ -23,15 +23,13 @@ resource "azurerm_resource_group" "rg-app" {
   tags = var.tags
 }
 
-resource "azurerm_app_service_plan" "plan-app" {
+resource "azurerm_service_plan" "plan-app" {
   name                = "${var.service_plan_name}-${var.environment}"
   location            = azurerm_resource_group.rg-app.location
   resource_group_name = azurerm_resource_group.rg-app.name
 
-  sku {
-    tier = "Standard"
-    size = "S1"
-  }
+  os_type = "Windows"
+  sku_name = "S1"
 }
 
 locals {
@@ -40,15 +38,17 @@ locals {
   }
 }
 
-resource "azurerm_app_service" "app" {
+resource "azurerm_windows_web_app" "app" {
   name                = "${var.app_name}-${var.environment}"
   location            = azurerm_resource_group.rg-app.location
   resource_group_name = azurerm_resource_group.rg-app.name
-  app_service_plan_id = azurerm_app_service_plan.plan-app.id
+  service_plan_id     = azurerm_service_plan.plan-app.id
 
   site_config {
-    dotnet_framework_version = "v4.0"
-    scm_type                 = "LocalGit"
+    application_stack {
+      current_stack  = "dotnet"
+      dotnet_version = "v4.0"
+    }
   }
 
   app_settings = merge(local.default_app_settings, var.custom_app_settings)
