0742e76a61a86086361dafd1c88306ef15c9cae3
mikaelkrief <krief_mikael@hotmail.com>
2022-10-23 12:57:19 +0000

    add

---
+++ CHAP05/remote-state/main.tf
@@ -0,0 +1,36 @@
+
+terraform {
+  required_version = ">= 0.12"
+}
+
+provider "azurerm" {
+  features {
+  }
+}
+
+
+resource "azurerm_resource_group" "rg-app" {
+  name     = "${var.resource_group_name}-${var.environment}"
+  location = var.location
+  tags = {
+    ENV = var.environment
+  }
+}
+
+
+data "terraform_remote_state" "service_plan_tfstate" {
+  backend = "azurerm"
+  config = {
+    resource_group_name  = "rg_tfstate"
+    storage_account_name = "storstate"
+    container_name       = "tfbackends"
+    key                  = "serviceplan.tfstate"
+  }
+}
+
+resource "azurerm_app_service" "app" {
+  name                = "${var.app_name}-${var.environment}"
+  location            = azurerm_resource_group.rg-app.location
+  resource_group_name = azurerm_resource_group.rg-app.name
+  app_service_plan_id = data.terraform_remote_state.service_plan_tfstate.service_plan_id
+}
---
+++ CHAP05/remote-state/terraform.tfvars
@@ -0,0 +1,6 @@
+resource_group_name = "RG-App"
+service_plan_name = "Plan-App"
+environment = "DEV1"
+custom_app_settings = {
+    APP = "1"
+}
