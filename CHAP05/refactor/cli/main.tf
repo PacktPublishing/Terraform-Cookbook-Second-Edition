679b53836014d6d8fd44ab5689b81b7d49cc2dde
mikaelkrief <krief_mikael@hotmail.com>
2022-12-04 14:35:50 +0000

    refactor chap05

---
+++ CHAP05/refactor/cli/main.tf
@@ -0,0 +1,27 @@
+
+terraform {
+  required_version = "~> 1.0"
+  required_providers {
+    random = {
+      source  = "hashicorp/random"
+      version = "3.4.3"
+    }
+  }
+}
+
+locals {
+  random_list = [16, 5]
+}
+
+# resource "random_string" "str" {
+#   length           = 16
+# }
+
+# resource "random_string" "str2" {
+#   length           = 5
+# }
+
+resource "random_string" "strfinal" {
+  count  = length(local.random_list)
+  length = local.random_list[count.index]
+}
---
+++ CHAP05/refactor/moved/main.tf
@@ -0,0 +1,37 @@
+
+terraform {
+  required_version = "~> 1.0"
+  required_providers {
+    random = {
+      source  = "hashicorp/random"
+      version = "3.4.3"
+    }
+  }
+}
+
+locals {
+  random_list = [16, 5]
+}
+
+# resource "random_string" "str" {
+#   length           = 16
+# }
+
+# resource "random_string" "str2" {
+#   length           = 5
+# }
+
+resource "random_string" "strfinal" {
+  count  = length(local.random_list)
+  length = local.random_list[count.index]
+}
+
+# moved {
+#   from = random_string.str
+#   to   = random_string.strfinal[0]
+# }
+
+# moved {
+#   from = random_string.str2
+#   to   = random_string.strfinal[1]
+# }
