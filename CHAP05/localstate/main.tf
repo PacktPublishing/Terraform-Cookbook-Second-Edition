7a97c7b8c92aadde3cec1fda3d641011d92a6aa1
Radek Simko <radek.simko@gmail.com>
2022-12-11 18:24:36 +0000

    add/set appropriate provider & TF constraints

--- CHAP05/localstate/main.tf
+++ CHAP05/localstate/main.tf
@@ -2,6 +2,14 @@ terraform {
   backend "local" {
     path = "../../demo.tfstate"
   }
+
+  required_version = "~> 1.0"
+  required_providers {
+    random = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.4"
+    }
+  }
 }
 
 resource "random_password" "password" {
--- CHAP05/managestate/main.tf
+++ CHAP05/managestate/main.tf
@@ -1,6 +1,12 @@
 
 terraform {
-  required_version = ">= 0.12"
+  required_version = "~> 1.0"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.20"
+    }
+  }
 }
 
 provider "azurerm" {
