d01219188887ee6fdd06a419be7e8c0ac8cfde9b
Mikael Krief <mickael.krief@health-data-hub.fr>
2022-12-12 21:45:34 +0100

    add

---
+++ CHAP06/sampleApp/main.tf
@@ -0,0 +1,61 @@
+terraform {
+  required_version = "~> 1.1"
+  required_providers {
+    azurerm = {
+      version = "~> 3.23"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+locals {
+  common_app_settings = {
+    "INSTRUMENTATIONKEY" = azurerm_application_insights.appinsight-app.instrumentation_key
+  }
+}
+
+resource "azurerm_resource_group" "rg-app" {
+  name     = "${var.resource_group_name}-${var.environment}"
+  location = var.location
+  tags = {
+    ENV = var.environment
+  }
+}
+
+resource "azurerm_service_plan" "plan-app" {
+  name                = "${var.service_plan_name}-${var.environment}"
+  location            = azurerm_resource_group.rg-app.location
+  resource_group_name = azurerm_resource_group.rg-app.name
+
+  os_type = "Windows"
+  sku_name = "S1"
+
+  tags = {
+    ENV       = var.environment
+    CreatedBy = var.createdby
+  }
+}
+
+resource "azurerm_linux_web_app" "app" {
+  name                = "${var.app_name}-${var.environment}"
+  location            = azurerm_resource_group.rg-app.location
+  resource_group_name = azurerm_resource_group.rg-app.name
+  service_plan_id = azurerm_service_plan.plan-app.id
+
+  site_config {}
+}
+
+resource "azurerm_application_insights" "appinsight-app" {
+  name                = "${var.app_name}-${var.environment}"
+  location            = azurerm_resource_group.rg-app.location
+  resource_group_name = azurerm_resource_group.rg-app.name
+  application_type    = "web"
+
+  tags = {
+    ENV       = var.environment
+    CreatedBy = var.createdby
+  }
+}
