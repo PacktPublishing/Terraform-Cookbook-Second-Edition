ae5159b1dc2d74221a6661649664dd9667e1cb9d
mikaelkrief <krief_mikael@hotmail.com>
2023-01-01 14:49:25 +0000

    add

--- CHAP08/feature/main.tf
+++ CHAP08/feature/main.tf
@@ -9,20 +9,15 @@ terraform {
 }
 
 provider "azurerm" {
-  features {}
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+  }
 }
 
 
 resource "azurerm_resource_group" "rg-app" {
   name     = "rg-demo-feature"
   location = "westeurope"
-}
-
-resource "azurerm_storage_account" "sa" {
-  name                     = "satestfeature11"
-  resource_group_name      = azurerm_resource_group.rg-app.name
-  location                 = "westeurope"
-  account_tier             = "Standard"
-  account_kind             = "StorageV2"
-  account_replication_type = "GRS"
 }
