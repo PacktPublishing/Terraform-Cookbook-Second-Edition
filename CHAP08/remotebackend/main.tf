bd51ad942d9ed7503d7a9c39e65577f69249c9d0
mikaelkrief <krief_mikael@hotmail.com>
2023-01-14 18:30:36 +0000

    update

--- CHAP08/remotebackend/main.tf
+++ CHAP08/remotebackend/main.tf
@@ -1,16 +1,23 @@
-provider "azurerm" {
-  features {}
-}
-
 terraform {
-  required_version = ">= 1.0"
+  required_version = "~> 1.1"
   backend "azurerm" {
     resource_group_name  = "RG-TFBACKEND"
     storage_account_name = "storagetfbackend"
     container_name       = "tfstate"
     key                  = "myapp.tfstate"
   }
+  required_providers {
+    azurerm = {
+      version = "~> 3.23"
+    }
+  }
 }
+
+provider "azurerm" {
+  features {}
+}
+
+
 
 resource "azurerm_resource_group" "rg" {
   name     = "RG-demo"
