22833d10f90667756c55f1c427cb928eef727db3
mikaelkrief <krief_mikael@hotmail.com>
2023-02-15 21:45:50 +0000

    add

---
+++ CHAP10/kind/main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    kind = {
+      source = "tehcyx/kind"
+      version = "0.0.16"
+    }
+  }
+}
+
+provider "kind" {}
+
+locals {
+    k8s_config_path = pathexpand("~/.kube/kindconfig")
+}
+
+resource "kind_cluster" "default" {
+  name = "cluster-book"
+  wait_for_ready = true
+  kubeconfig_path = local.k8s_config_path
+}
