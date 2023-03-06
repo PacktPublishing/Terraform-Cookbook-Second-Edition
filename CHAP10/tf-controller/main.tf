cd468e41d1f93400c526499f31f6203d186de481
mikaelkrief <krief_mikael@hotmail.com>
2023-03-06 05:21:22 +0000

    add

---
+++ CHAP10/tf-controller/main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.7.1"
+    }
+  }
+}
+
+provider "helm" {
+  kubernetes {
+    config_path = pathexpand("~/.kube/config")
+  }
+}
+
+resource "helm_release" "tf-controller" {
+  name             = "tf-controller"
+  repository       = "https://weaveworks.github.io/tf-controller/"
+  chart            = "tf-controller"
+  namespace        = "flux-system"
+  wait             = true
+}
