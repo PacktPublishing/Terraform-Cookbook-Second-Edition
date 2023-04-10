a0b8261441f63f48576f6034bab477ccdcb3089c
mikaelkrief <krief_mikael@hotmail.com>
2023-04-10 14:36:47 +0000

    update docker go

--- CHAP11/testing-terratest/module/runtests.sh
+++ CHAP11/testing-terratest/module/runtests.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 echo "==> Get terratest package"
-go get github.com/gruntwork-io/terratest/modules/terraform
+go mod init github.com/terraform-cookbook/module-test
+go mod tidy
 echo "==> go test"
 go test -v ./tests/ -timeout 30m
