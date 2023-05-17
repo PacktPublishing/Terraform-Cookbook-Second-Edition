8a3ed3e575e803227ba5e7832b513be8ae60f5c1
mikaelkrief <krief_mikael@hotmail.com>
2023-05-17 11:38:11 +0000

    update

--- CHAP11/testing-terratest/module/runtests.sh
+++ CHAP11/testing-terratest/module/runtests.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "==> Get terratest package"
-go mod init github.com/terraform-cookbook/module-test
+go mod init github.com/terraform-cookbook-second-edition/module-test
 go mod tidy
 echo "==> go test"
 go test -v ./tests/ -timeout 30m
