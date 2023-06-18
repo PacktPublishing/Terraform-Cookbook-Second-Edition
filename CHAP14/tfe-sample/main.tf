b473d649535b892354edc1022352c77d37407adf
mikaelkrief <krief_mikael@hotmail.com>
2023-06-18 15:01:19 +0000

    add

---
+++ CHAP14/tfe-sample/main.tf
@@ -0,0 +1,40 @@
+terraform {
+  required_version = ">= 1.1.0"
+  required_providers {
+    tfe = {
+      source  = "hashicorp/tfe"
+      version = "0.45.0"
+    }
+  }
+  cloud {
+    hostname     = "app.terraform.io"
+    organization = "demo-tfe"
+
+    workspaces {
+      name = "manage-tfe"
+    }
+  }
+}
+
+provider "tfe" {
+}
+
+resource "tfe_organization" "test-organization" {
+  name  = "demo-tfe-book"
+  email = "admin@company.com"
+}
+
+resource "tfe_project" "test" {
+  organization = tfe_organization.test-organization.name
+  name         = "appproject"
+}
+
+resource "tfe_workspace" "wsnetwork" {
+  name         = "network"
+  organization = tfe_organization.test-organization.name
+}
+
+resource "tfe_workspace" "wsvm" {
+  name         = "vm"
+  organization = tfe_organization.test-organization.name
+}
