add7959180bf54850abb5cdbe44aaacef71055ee
mikaelkrief <krief_mikael@hotmail.com>
2023-08-17 16:13:08 +0000

    update

--- CHAP07/moduledemo/Modules/webapp/main.tf
+++ CHAP07/moduledemo/Modules/webapp/main.tf
@@ -1,4 +1,21 @@
+terraform {
+  required_version = "~> 1.0"
+  required_providers {
+    azurerm = {
+      version = "~> 3.18"
+    }
+  }
+}
 
+provider "azurerm" {
+  features {}
+}
+
+resource "random_string" "str" {
+  length  = 4
+  special = false
+  upper   = false
+}
 
 resource "azurerm_service_plan" "plan-app" {
   name                = var.service_plan_name
@@ -9,7 +26,7 @@ resource "azurerm_service_plan" "plan-app" {
 }
 
 resource "azurerm_linux_web_app" "app" {
-  name                = var.app_name
+  name                = "${var.app_name }-${random_string.str.result}"
   location            = var.location
   resource_group_name = var.resource_group_name
   service_plan_id     = azurerm_service_plan.plan-app.id
