87a8b2cd760fbee74b60149c5eb6307c16c670e6
mikaelkrief <krief_mikael@hotmail.com>
2023-08-22 10:00:44 +0000

    fix chap01

--- CHAP01/terraform-docker/main.tf
+++ CHAP01/terraform-docker/main.tf
@@ -1,3 +1,13 @@
+terraform {
+  required_version = "~> 1.0"
+  required_providers {
+    random = {
+      source  = "hashicorp/random"
+      version = "3.5.1"
+    }
+  }
+}
+
 resource "random_string" "random" {
   length = 16
 }
--- CHAP01/tf011/main.tf
+++ CHAP01/tf011/main.tf
@@ -1,7 +1,18 @@
+terraform {
+  required_version = "~> 1.0"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.20"
+    }
+  }
+}
+
 provider "azurerm" {}
 
 variable "resource_group_name" {
   default = "rg_test"
+  type    = string
 }
 
 resource "azurerm_resource_group" "rg" {
